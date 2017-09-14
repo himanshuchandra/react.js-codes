818bfc3d96fc7facb41570c71126a7b19d596c68
Himanshu_Chandra <himanshu.chandra@outlook.com>
2017-09-14 23:56:37 +0530

    'ES6'

---
+++ Codes 2/1 ES6/app/module1.js
@@ -0,0 +1,17 @@
+const calcObject = {
+    add(x, y) {
+        return x + y;
+    },
+    subtract(x, y) {
+        return x - y;
+    }
+}
+
+function show(){
+    console.log('this is show');
+}
+
+const MAX =1000;
+
+export {calcObject,MAX};
+// export default caclcObject; //single object export
---
+++ Codes 2/1 ES6/app/module2.js
@@ -0,0 +1,5 @@
+import {calcObject ,MAX} from './module1'; //Multiple objects import
+// import calcObject from './module1'; //single import
+
+console.log("Add is"+ calcObject.add(100,200));
+console.log(`MAX is ${MAX}`);
